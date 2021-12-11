0c677606f97c090e9db7e3cc44f8c7c0cf2aa0d5
keshav suman <keshavsuman96@gmail.com>
2021-12-11 14:12:17 +0530

    sdg

--- models/forms/cruspoFormSubmissionModel.js
+++ models/forms/cruspoFormSubmissionModel.js
@@ -4,13 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose_1 = __importDefault(require("mongoose"));
+var formSubmissionSchema = new mongoose_1.default.Schema({}, { strict: false });
 function cruspoFormSubmissions(prefix) {
     var cruspoForm;
     try {
         cruspoForm = mongoose_1.default.model(prefix + "_formSubissions");
     }
     catch (error) {
-        cruspoForm = mongoose_1.default.model(prefix + "_formSubmissions", new mongoose_1.default.Schema({}, { strict: false }));
+        cruspoForm = mongoose_1.default.model(prefix + "_formSubmissions", formSubmissionSchema);
     }
     return cruspoForm;
 }

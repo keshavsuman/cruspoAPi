b1c3226e397c9482d3cc261f745f10e61b8d65fb
keshav suman <keshavsuman96@gmail.com>
2022-01-13 14:37:01 +0530

    asgasfhbadtjdyh

--- keshavsuman.cruspo.com.js
+++ keshavsuman.cruspo.com.js
@@ -8,18 +8,18 @@ var mongoose_1 = __importDefault(require("mongoose"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var path_1 = __importDefault(require("path"));
 var cors_1 = __importDefault(require("cors"));
+// const authenticationRoutes = require('./Routes/authenticationRoutes');
+var goToPageRoutes = require('./Routes/gotopageRoutes');
 var paymentRoutes = require('./Routes/paymentRoutes');
 var bookAppointmentRoutes = require('./Routes/bookAppointmentRoutes');
 var formRoutes = require('./Routes/formRoutes');
+// const manageMembersRoutes  = require('./Routes/manageMembersRoutes');   
 var apiRoutes = require('./Routes/API/apiRoutes');
 var app = express_1.default();
 dotenv_1.default.config({
     path: path_1.default.resolve(__dirname, '.env')
 });
-app.use(cors_1.default({
-    origin: '*',
-    optionsSuccessStatus: 200
-}));
+app.use(cors_1.default());
 mongoose_1.default.connect(String(process.env.DB_URL), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose_1.default.connection.on('error', function (error) {
     console.log(error);
@@ -35,9 +35,12 @@ app.use(express_1.default.urlencoded({
     extended: true
 }));
 app.use('/api', apiRoutes);
+app.use('/goToPage', goToPageRoutes);
 app.use('/form', formRoutes);
+// app.use('/authentication',authenticationRoutes);
 app.use('/payment', paymentRoutes);
 app.use('/bookAppointment', bookAppointmentRoutes);
+// app.use('/manageMembers',manageMembersRoutes);
 app.listen(process.env.PORT, function () {
-    console.log("Server is running in port 5000");
+    console.log("Server is running in port " + process.env.PORT);
 });

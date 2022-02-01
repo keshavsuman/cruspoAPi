84bfa5d7acb9cee6ade722db83cf3b37395aaa13
keshav suman <keshavsuman96@gmail.com>
2022-02-01 23:03:52 +0530

    sagzh

--- models/form/formSubmissionModel.js
+++ models/form/formSubmissionModel.js
@@ -1,7 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
+var mongoose_1 = __importDefault(require("mongoose"));
 function formSubmissionModel(prefix) {
-    return (0, mongoose_1.model)("".concat(prefix, "_formSubmission"), new mongoose_1.Schema({}, { strict: false, timestamps: true }));
+    var model;
+    try {
+        model = mongoose_1.default.model("".concat(prefix, "_formSubmission"));
+    }
+    catch (error) {
+        model = mongoose_1.default.model("".concat(prefix, "_formSubmission"), new mongoose_1.default.Schema({}, { strict: false, timestamps: true }));
+    }
+    return model;
 }
 exports.default = formSubmissionModel;
